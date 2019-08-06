44f485390c4ac78034278c94c05ea2d8760f130c
李鹏飞 <305086028@qq.com>
2019-08-06 22:44:00 +0800

    上传代码

---
+++ 类和对象/Date.h
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma once
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	int GetMonthDay(int year,int month)
+	{
+		int array[] = {};
+
+	}
+	Date(int year = 1900, int month = 1, int day = 1)
+	{
+
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
