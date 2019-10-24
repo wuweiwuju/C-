df90a722630d9d05d936d672b9fc09341d8a8c15
李鹏飞 <305086028@qq.com>
2019-10-24 21:51:49 +0800

    上传代码

---
+++ 557反转字符串III/test.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+	string reverseWords(string s) {
+		string::iterator it = s.begin();
+		int count = 0;
+		while (it < s.end())
+		{
+			if (*it != ' ')
+			{
+				count = s.find(' ', count + 1);
+				if (count == s.npos)
+				{
+					reverse(it, s.end());
+					return s;
+				}
+				reverse(it, s.begin() + count);
+				it = s.begin() + count;
+			}
+			else
+			{
+				it++;
+			}
+		}
+		return s;
+	}
+};
